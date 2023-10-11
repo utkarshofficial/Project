7c69711436e6c5ac1ed940d7ecf060b2041504e0
Utkarsh Sharma <83834666+utkarshofficial@users.noreply.github.com>
2023-10-11 10:27:10 +0530

    lines of code

--- CE1/temp.py
+++ CE1/temp.py
@@ -1,13 +1,36 @@
-def convert(i):
-    return '''
-    <img
-    class="markings2"
-    src="./src/images/bare_raber2marked_{0}.png";
-    />
-    '''.format(i)
+# def convert(i):
+#     return '''
+#     <img
+#     class="markings2"
+#     src="./src/images/bare_raber2marked_{0}.png";
+#     />
+#     '''.format(i)
 
-all = ""
-for i in range(1, 18):
-    all = all + convert(i)
+# all = ""
+# for i in range(1, 18):
+#     all = all + convert(i)
 
-print(all)
+# print(all)
+
+# project files line
+s = '''72
+15
+44
+249
+1137
+28
+1862
+123
+39
+85
+141
+117
+106
+162'''
+
+s = s.split('\n')
+num = 0
+for i in s:
+    num = num + int(i)
+
+print(num)
